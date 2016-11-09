487b8bb430952f4bb466e0b7dabf32c04672b805
Rajyalakshmi C <chamarthi226@gmail.com>
2016-11-09 09:04:29 -0600

    data inserted into tables

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name,devoured) VALUES ('Cheese Burger',true);
+INSERT INTO burgers(burger_name,devoured) VALUES ('Chicken Burger',true);
+INSERT INTO burgers(burger_name,devoured) VALUES ('HamBurger',false); 
+INSERT INTO burgers(burger_name,devoured) VALUES ('Deep-Fried burger',false); 
+/*select * from burgers;*/
